689184cee978517db45179cf56f4f39a8e4fa94b
ClaretJack <jack_claret@hotmail.co.uk>
2024-04-16 12:30:32 +0100

    Task 4 endpoint api/articles/:article_id complete, happy and sad path for tests done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics_controller");
 const { getAllEndpoints } = require("./controllers/api_controller");
+const { getArticleById } = require("./controllers/article_controller");
 
 const app = express();
 app.use(express.json());
@@ -8,5 +9,20 @@ app.use(express.json());
 app.get("/api/topics", getTopics);
 
 app.get("/api", getAllEndpoints);
+
+app.get("/api/articles/:article_id", getArticleById);
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request" });
+  }
+});
 
 module.exports = app;
