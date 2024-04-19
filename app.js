8261c21ea78c8e9646974785506167eeb6fbf1d8
ClaretJack <jack_claret@hotmail.co.uk>
2024-04-19 15:31:53 +0100

    refactor a test

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ app.delete("/api/comments/:comment_id", deleteCommentById);
 ////////////////////////////////// error handling below
 
 //custom
+
+app.all("*", (req, res, next) => {
+  res.status(404).send({ msg: "Path not found" });
+});
+
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
